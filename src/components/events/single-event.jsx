13d83d1ff2141686c1506dfb6cec71090bca387a
Varun Chaubey <varunchaubey4@gmail.com>
2023-05-24 07:29:06 +0530

    Add styling for email registration page

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const SingleEvent = ({ data }) => {
+  const onSubmit = () => {};
   return (
-    <div>
+    <div className="event_single_page">
       <Image src={data.image} alt={data.title} width={1000} height={500} />
       <h1>{data.title}</h1>
       <p>{data.description}</p>
-
-      <input type="email" /><button>Submit</button>
+      <form onSubmit={onSubmit} className="email_registration">
+        <label>Get Registered for this event!</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Please insert your email here"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
 
-export default SingleEvent
+export default SingleEvent;
