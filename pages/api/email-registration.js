d5e2c983cf70dd46c00dea25d7597b742f4b9f03
Varun Chaubey <varunchaubey4@gmail.com>
2023-05-24 09:43:39 +0530

    Add api for email registration

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,3 +1,10 @@
-export default function handler (req, res) {
-    const {method} = req;
+export default function handler(req, res) {
+  const { method } = req;
+  if (method === "POST") {
+    return res
+      .status(200)
+      .json({
+        message: `You has been registered successfully with the email: ${req.body.email}${req.body.eventId}`
+      });
+  }
 }
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const SingleEvent = ({ data }) => {
-  const onSubmit = () => {};
+  const inputEmail = useRef();
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+    try {
+      const response = await fetch("/api/email-registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if (!response.ok) throw new Error(`Error: ${response.status}`);
+      const data = await response.json();
+      console.log("POST", data);
+    } catch (e) {
+      console.log("ERROR", e);
+    }
+  };
   return (
     <div className="event_single_page">
       <Image src={data.image} alt={data.title} width={1000} height={500} />
@@ -11,6 +34,7 @@ const SingleEvent = ({ data }) => {
       <form onSubmit={onSubmit} className="email_registration">
         <label>Get Registered for this event!</label>
         <input
+          ref={inputEmail}
           type="email"
           id="email"
           placeholder="Please insert your email here"
