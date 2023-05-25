a325b3c15f5022ae24f92dc3d3ea1fa9e2fb08b6
Varun Chaubey <varunchaubey4@gmail.com>
2023-05-25 09:13:27 +0530

    Add email registration functionality and validator

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,10 +1,58 @@
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+  return path.join(process.cwd(), "data", "data.json");
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  return JSON.parse(jsonData);
+}
+
 export default function handler(req, res) {
   const { method } = req;
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
   if (method === "POST") {
-    return res
-      .status(200)
-      .json({
-        message: `You has been registered successfully with the email: ${req.body.email}${req.body.eventId}`
+    const { email, eventId } = req.body;
+
+    if(!email | !email.includes('@')) {
+      res.status(422).json({message: 'Invalid email address'});
+      return;
+    }
+
+    const newAllEvents = allEvents.map((ev) => {
+      if (ev.id == eventId) {
+        if (ev.emails_registered.includes(email)) {
+          res
+            .status(409)
+            .json({ message: "This email has already been registered" });
+          return ev;
+        }
+        return {
+          ...ev,
+          emails_registered: [...ev.emails_registered, email],
+        };
+      }
+      return ev;
+    });
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents })
+    );
+
+    if (!allEvents) {
+      return res.status(200).json({
+        message: "Events data not found",
       });
+    }
+
+    return res.status(200).json({
+      message: `You have been registered successfully with the email: ${email} for the event: ${eventId}`,
+    });
   }
 }
--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -1,15 +1,23 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
 const SingleEvent = ({ data }) => {
   const inputEmail = useRef();
   const router = useRouter();
+  const [message, setMessage] = useState();
 
   const onSubmit = async (e) => {
     e.preventDefault();
     const emailValue = inputEmail.current.value;
     const eventId = router?.query.id;
+
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(!emailValue.match(validRegex)) {
+      setMessage('Please introduce a correct email address');
+    }
+
     try {
       const response = await fetch("/api/email-registration", {
         method: "POST",
@@ -21,7 +29,8 @@ const SingleEvent = ({ data }) => {
 
       if (!response.ok) throw new Error(`Error: ${response.status}`);
       const data = await response.json();
-      console.log("POST", data);
+      setMessage(data.message);
+      inputEmail.current.value = '';
     } catch (e) {
       console.log("ERROR", e);
     }
@@ -35,12 +44,12 @@ const SingleEvent = ({ data }) => {
         <label>Get Registered for this event!</label>
         <input
           ref={inputEmail}
-          type="email"
           id="email"
           placeholder="Please insert your email here"
         />
         <button type="submit">Submit</button>
       </form>
+      <p>{message}</p>
     </div>
   );
 };
